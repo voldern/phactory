5620c8813508b0e7c3d487ab583db2f45cb0ede6
Espen Volden <voldern@hoeggen.net>
2013-03-03 21:46:44 +0100

    Added MongoId example

---
+++ examples/mongoid.php
@@ -0,0 +1,27 @@
+<?php
+require_once __DIR__ . '/../library/autoloader.php';
+
+spl_autoload_register('autoload');
+
+class MongoIdFactory extends \Phactory\Phactory {
+    protected $fields = array(
+        'staticField' => array(
+            'type' => '\Phactory\Type\MongoId',
+            'generator' => 'static',
+            'value' => '5133b68b48336e08b0000000'
+        ),
+        'randomField' => array(
+            'type' => '\Phactory\Type\MongoId',
+            'generator' => 'random'
+        ),
+        'randomStaticValues' => array(
+            'type' => '\Phactory\Type\MongoId',
+            'generator' => 'random',
+            'values' => array('5133b62d48336e37ae000000', '5133b64d48336e65ae000000',
+                '5133b67b48336e3caf000000')
+        )
+    );
+}
+
+$fact = new MongoIdFactory();
+var_dump($fact->generate(1));
