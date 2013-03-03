0ec512d9c1ffaed43b10d2e40562e71c107de2f9
Espen Volden <voldern@hoeggen.net>
2013-03-03 21:32:37 +0100

    Add datetime example

---
+++ examples/datetime.php
@@ -0,0 +1,27 @@
+<?php
+require_once __DIR__ . '/../library/autoloader.php';
+
+spl_autoload_register('autoload');
+
+class DateTimeFactory extends \Phactory\Phactory {
+    protected $fields = array(
+        'staticField' => array(
+            'type' => '\Phactory\Type\DateTime',
+            'generator' => 'static',
+            'value' => 'now'
+        ),
+        'randomField' => array(
+            'type' => '\Phactory\Type\DateTime',
+            'generator' => 'random',
+            'range' => array('min' => 'now', 'max' => '+1 day')
+        ),
+        'randoStaticValues' => array(
+            'type' => '\Phactory\Type\DateTime',
+            'generator' => 'random',
+            'static' => array('now', '+1 day', '+2 day', '+1 year')
+        )
+    );
+}
+
+$fact = new DateTimeFactory();
+var_dump($fact->generate(1));
